0b0d51d1fdd0c165e3f0933019a4d5ab2b353c3f
Jesus Ventura <jesusventura2005@gmail.com>
2025-02-24 22:41:22 -0400

    Update changes

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+import NiceSelect from 'nice-select2';
+
+// Initialize NiceSelect
+document.addEventListener('DOMContentLoaded', () => {
+    new NiceSelect(document.getElementById('a-select'));
+    new NiceSelect(document.getElementById('categorie_select'));
+});
+
