f978912b2324e96817d9cd2eec2a9f0435c864e3
Jesus Ventura <jesusventura2005@gmail.com>
2025-02-25 21:42:55 -0400

    upgrateds

--- src/main.js
+++ src/main.js
@@ -1,76 +1,90 @@
 import NiceSelect from "nice-select2";
 import { APIKEY } from "./secret";
 
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  new NiceSelect(document.getElementById("a-select"));
-  new NiceSelect(document.getElementById("categorie_select"));
+document.addEventListener('DOMContentLoaded', function () {
+    const selectElementnice = new NiceSelect(document.getElementById("a-select"));
+    const selectElementnice2 = new NiceSelect(document.getElementById("categorie_select"));
 });
 
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_URL = `${BASE_URL}discover/movie?api_key=${APIKEY}&language=es-ES`;
-
 const section = document.querySelector("section");
+const selectElement = document.getElementById("a-select");
+const selectCategory = document.getElementById("categorie_select")
+
+let content = selectElement.value;
+
+let categorie = selectCategory.value;
+
+
+selectCategory.addEventListener("change",  function () {
+  categorie = selectCategory.value;
+
+  const divsAEliminar = section.querySelectorAll("div#divmovie");
+
+  divsAEliminar.forEach((div) => {
+      div.remove();
+  });
+
+  main();
+});
+
+selectElement.addEventListener("change",  function () {
+    content = selectElement.value;
+    const divsAEliminar = section.querySelectorAll("div#divmovie");
+
+    divsAEliminar.forEach((div) => {
+        div.remove();
+    });
+
+    main();
+});
+
+const BASE_URL = "https://api.themoviedb.org/3/";
 
 const fetchData = async (api) => {
-  try {
-    const res = await fetch(api);
-    const data = await res.json();
-    console.log(data.results);
-    return data;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+    try {
+        const res = await fetch(api);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
 const componentMovie = (movie) => {
-  const divMovie = document.createElement('div')  
-  const imgMovie = document.createElement('div');
-  const h2Movie = document.createElement('h2');
+    const divMovie = document.createElement("div");
+    const imgMovie = document.createElement("div");
+    const h2Movie = document.createElement("h2");
 
-  imgMovie.id = "imgmovie"
+    imgMovie.id = "imgmovie";
+    divMovie.id = "divmovie";
 
-  divMovie.id = "divmovie"
+    imgMovie.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`;
 
-  imgMovie.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`
-  imgMovie.style.backgroundSize = 'cover'; // Ajusta el tamaño de la imagen
-  imgMovie.style.backgroundPosition = 'center'; // Centra la imagen
-  imgMovie.style.backgroundRepeat = 'no-repeat'; // Evita la repetición de la imagen
+    h2Movie.textContent = movie.title || movie.name;
 
+    divMovie.appendChild(imgMovie);
+    divMovie.appendChild(h2Movie);
 
-  h2Movie.textContent = movie.title;
-
-  divMovie.appendChild(imgMovie);
-  divMovie.appendChild(h2Movie);
-
-
-  return divMovie
-
-
+    return divMovie;
 };
 
-const renderMovies = async () => {
-  const moviesData = await fetchData(API_URL);
-  if(moviesData && moviesData.results){
-      const moviesArray = moviesData.results;
+const renderMovies = async (page) => {
+    const API_URL = `${BASE_URL}discover/${content}?api_key=${APIKEY}&language=es-ES&page=${page}&with_genres=${categorie}`;
+    const moviesData = await fetchData(API_URL);
+    if (moviesData && moviesData.results) {
+        const moviesArray = moviesData.results;
 
-      moviesArray.forEach((movie) => {
-        section.append(componentMovie(movie))
-        
-      });
-  } else {
-      console.log("No se pudieron obtener datos de peliculas");
-  }
-
+        moviesArray.forEach((movie) => {
+            section.append(componentMovie(movie));
+        });
+    } else {
+        console.log("No se pudieron obtener datos de peliculas");
+    }
 };
 
-renderMovies();
+const main = () => {
+    renderMovies(1);
+};
 
-
-
-
-
-
-
+main();
