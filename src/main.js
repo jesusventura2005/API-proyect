69dbeb13f437f2244c5de9c209fac4972f298b90
Jesus Ventura <jesusventura2005@gmail.com>
2025-03-06 22:31:59 -0400

    fix category

--- src/main.js
+++ src/main.js
@@ -8,27 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const selectElement = document.getElementById("a-select");
   const selectCategory = document.getElementById("categorie_select");
   const description = document.querySelector(".description");
+  const search = document.querySelector("#search");
 
-let page = 1;
+  let page = 1;
 
-window.addEventListener("scroll", () => {
+  window.addEventListener("scroll", () => {
     const scrollHeight = Math.max(
-        document.documentElement.scrollHeight,
-        document.body.scrollHeight
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight
     );
     const windowHeight = window.innerHeight;
     const scrollY = window.scrollY;
 
     if (scrollY + windowHeight >= scrollHeight - 10) {
-        page++;
-        renderMovies(page);
+      page++;
+      renderMovies(page);
     }
-});
-
-
+  });
 
   new NiceSelect(selectElement);
-  new NiceSelect(selectCategory);
+  let niceSelectCategory = new NiceSelect(selectCategory);
 
   let content = selectElement.value;
   let categorie = "";
@@ -36,16 +35,36 @@ window.addEventListener("scroll", () => {
   const handleSelectChange = () => {
     const divsAEliminar = section.querySelectorAll("div#divmovie");
     divsAEliminar.forEach((div) => div.remove());
-    main();
+    renderMovies(1);
   };
+
+  const handleSearchChange = (word) => {
+    const divsAEliminar = section.querySelectorAll("div#divmovie");
+    divsAEliminar.forEach((div) => div.remove());
+    renderSearchMovies(word);
+  };
+
+  search.addEventListener("keyup", () => {
+    let word = search.value;
+
+    console.log(word.length);
+
+    if (word.length == 0) {
+      handleSelectChange();
+    } else {
+      handleSearchChange(word);
+    }
+  });
 
   selectCategory.addEventListener("change", function () {
     categorie = selectCategory.value;
     handleSelectChange();
   });
 
-  selectElement.addEventListener("change", function () {
+  selectElement.addEventListener("change", async function () {
+    niceSelectCategory.destroy();
     content = selectElement.value;
+    await changeCategories();
     handleSelectChange();
   });
 
@@ -63,7 +82,31 @@ window.addEventListener("scroll", () => {
     }
   };
 
-  const createDescription = (voteAverage, voteCount, overiew, backdrop) => {
+  const changeCategories = async () => {
+    const data = await fetchData(
+      `${BASE_URL}genre/${content}/list?api_key=${APIKEY}&language=es-ES`
+    );
+
+    if (data && data.genres) {
+      selectCategory.innerHTML = ""; // Limpiar las opciones anteriores
+
+      data.genres.forEach((genre) => {
+        const option = document.createElement("option");
+        option.value = genre.id;
+        option.textContent = genre.name;
+        selectCategory.append(option);
+      });
+
+      // Esperar un pequeño tiempo antes de reinicializar NiceSelect
+
+        niceSelectCategory = new NiceSelect(selectCategory);
+
+    } else {
+      console.log("No se pudieron obtener las categorías");
+    }
+  };
+
+  const createDescription = (voteAverage, voteCount, overview, backdrop) => {
     const imgMovieDescription = document.createElement("div");
     const textMovieDescription = document.createElement("p");
     const descriptiondiv = document.createElement("div");
@@ -75,7 +118,6 @@ window.addEventListener("scroll", () => {
     const addBotton = document.createElement("button");
     addBotton.id = "addBotton";
     addBotton.textContent = "ADD";
-
 
     const bottonContainer = document.createElement("div");
     bottonContainer.id = "bottonContainer";
@@ -103,7 +145,7 @@ window.addEventListener("scroll", () => {
     imgMovieDescription.id = "imgMovieDescription";
     descriptiondiv.id = "description_div";
     imgMovieDescription.style.backgroundImage = backdrop;
-    textMovieDescription.textContent = overiew;
+    textMovieDescription.textContent = overview;
 
     containerRate.appendChild(rateDiv);
     containerRate.appendChild(stardiv);
@@ -117,8 +159,6 @@ window.addEventListener("scroll", () => {
     descriptiondiv.append(bottonContainer);
     description.append(descriptiondiv);
   };
-
-
 
   const componentMovie = (movie) => {
     const divMovie = document.createElement("div");
@@ -144,23 +184,16 @@ window.addEventListener("scroll", () => {
     divMovie.addEventListener("click", () => {
       containerDescription.classList.add("show");
 
-      
-
-
       createDescription(
         movieRateDescription,
         movieVoteCount,
         movieDescription,
         movieImageDescription
       );
-
-
     });
 
     outDescription.addEventListener("click", () => {
       containerDescription.classList.remove("show");
-
-
 
       const divsAEliminar = description.querySelectorAll("div");
       divsAEliminar.forEach((div) => div.remove());
@@ -169,9 +202,18 @@ window.addEventListener("scroll", () => {
     return divMovie;
   };
 
-
-
-
+  const renderSearchMovies = async (word) => {
+    const API_URL = `${BASE_URL}search/movie?query=${word}&page=1&include_adult=false&api_key=${APIKEY}`;
+    const moviesData = await fetchData(API_URL);
+    if (moviesData && moviesData.results) {
+      const moviesArray = moviesData.results;
+      moviesArray.forEach((movie) => {
+        section.append(componentMovie(movie));
+      });
+    } else {
+      console.log("No se pudieron obtener datos de peliculas");
+    }
+  };
 
   const renderMovies = async (page) => {
     const API_URL = `${BASE_URL}discover/${content}?api_key=${APIKEY}&language=es-ES&page=${page}&with_genres=${categorie}`;
